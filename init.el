41380c49befd84e7b50d9207ed4fef959f594b6a
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-12-18 19:42:07 -0500

    Unnecessary call to (package-initialize)

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
